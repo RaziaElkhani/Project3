ea4420d52ca326ef3d49bf3ce0b0b8b03e393b7b
RaziaElkhani <elkhanirazia@gmail.com>
2025-01-29 18:30:52 -0800

    Update DemoTest3.java

--- src/demo1/DemoTest3.java
+++ src/demo1/DemoTest3.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	System.out.println("Java is Fun");
 	
 }
-public DemoTest3(int a,int b) {
+public DemoTest3(int a,int b,int c) {
 	System.out.println("adding countracto");
 }
 }

e8fe3c5e9e1a0ce973c0cae979eaedb8938fcfb5
elkha <elkha@10.0.0.142>
2025-01-29 18:08:27 -0800

    adding counstructor

--- src/demo1/DemoTest3.java
+++ src/demo1/DemoTest3.java
@@ -3,5 +3,9 @@ package demo1;
 public class DemoTest3 {
 public static void main(String[] args) {
 	System.out.println("Java is Fun");
+	
+}
+public DemoTest3(int a,int b) {
+	System.out.println("adding countracto");
 }
 }
